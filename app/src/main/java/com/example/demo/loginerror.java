4a61f4dada5ff6b53e001cf95f23d1831d9482f4
Rajnish kumar <22f2000625@student.onlinedegree.iitm.ac.in>
2022-08-30 22:25:00 +0530

    Not the final one

---
+++ app/src/main/java/com/example/demo/loginerror.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demo.databinding.ActivityMainBinding;
+
+public class loginerror extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+
+
+    }
+}
